62c988ba49b3554af4bf40d9b493e1a50b58fe25
tiagomariinhoo <tiago.marinho.797@gmail.com>
2017-02-12 17:14:46 -0200

    Final

--- ProdAcademica/src/prodacademica/ProdAcademica.java
+++ ProdAcademica/src/prodacademica/ProdAcademica.java
@@ -100,8 +100,7 @@ public class ProdAcademica {
         } catch(Exception e){
             scan.nextLine();
             System.out.println("Por favor, tente novamente!");
-        } 
-        
+        }
     }
     
     
